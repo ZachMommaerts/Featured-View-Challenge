b09572c36ca49116d7f776796f15dd673eb9c703
ZachMommaerts <zmommaerts@gmail.com>
2023-04-02 17:43:05 -0500

    Initial Commit

---
+++ Featured View Challenge/Featured_View_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Featured_View_ChallengeApp.swift
+//  Featured View Challenge
+//
+//  Created by Zach Mommaerts on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Featured_View_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
