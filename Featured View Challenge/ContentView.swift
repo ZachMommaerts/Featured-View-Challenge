daa6ab23205bb77efbbd1869d9bd11d908ceb7ec
ZachMommaerts <zmommaerts@gmail.com>
2023-04-02 19:10:03 -0500

    - Complete functionality for app up to 50 cards

--- Featured View Challenge/ContentView.swift
+++ Featured View Challenge/ContentView.swift
@@ -9,13 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        GeometryReader { geo in
+            TabView {
+                ForEach(1...50, id: \.self){ index in
+                    let randRed = Double.random(in: 0..<1)
+                    let randGreen = Double.random(in: 0..<1)
+                    let randBlue = Double.random(in: 0..<1)
+                        ZStack{
+                            Rectangle()
+                                .foregroundColor(Color(red: randRed, green: randGreen, blue: randBlue))
+                                .cornerRadius(20)
+                                .shadow(radius: 10)
+                            Image(systemName: "\(index).circle")
+                                .resizable()
+                                .scaledToFit()
+                                .padding()
+                        }
+                        .frame(width: geo.size.width - 40, height: geo.size.height - 100)
+                }
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
         }
-        .padding()
     }
 }
 
